41562f2d894aca74b4147955327b3aebba031fd5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-03 00:34:32 +0530

    setup done

--- src/components/card/CardImage.jsx
+++ src/components/card/CardImage.jsx
@@ -1,8 +1,8 @@
 const CardImage = ({ src, alt }) => {
   return (
-    <div className="w-full h-48 overflow-hidden">
+    <div className="rounded-t-lg w-full h-48 overflow-hidden">
       {src ? (
-        <img src={src} alt={alt} className="w-full h-full object-cover rounded-t-lg mt-2" />
+        <img src={src} alt={alt} className="w-full h-full object-cover" />
       ) : (
         <div className="w-full h-full items-center justify-center flex bg-linear-to-br from-gray-200 to-gray-300">
           <span className="text-sm text-gray-400">No Image Available</span>
