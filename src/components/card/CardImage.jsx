58073bb3a74726e22cc36f0bd17e2e8dbc5f8f19
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-03 02:44:00 +0530

    setup done

--- src/components/card/CardImage.jsx
+++ src/components/card/CardImage.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
-const CardImage = () => {
+const CardImage = ({ src, alt = "Card Image" }) => {
   return (
-    <div>
-      
+    <div className="w-full h-48 overflow-hidden rounded-t-xl">
+      {src ? (
+        <img className="w-full h-full object-cover" src={src} alt={alt} />
+      ) : (
+        <div className=" bg-linear-to-br from-gray-100 to-gray-300">
+          <span>No Image available</span>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CardImage
+export default CardImage;
