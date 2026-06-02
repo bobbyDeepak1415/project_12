8ed8814a4e9e09d602f8618fced6d6ce8a879d66
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-02 16:21:06 +0530

    setup done

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 
 2. the card component accepts props like className for extra css classes,variant,padding,hover and children, we receive and dynamically render them from the app.jsx
 
-3. 
+3. created the baseStyles,which will be applied to everything
+
+4.
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,15 +5,31 @@ const Card = ({
   hover = true,
   className = "",
 }) => {
-  const baseStyles = "";
+  const baseStyles = "shadow-md rounded-lg transition-all duration-300";
 
-  const paddingStyles = {};
+  const paddingStyles = {
+    none: "",
+    small: "p-4",
+    normal: "p-6",
+    large: "p-8",
+  };
 
-  const variants = {};
+  const variants = {
+    default: "bg-gray-300 border-gray-800",
+    primary: "bg-blue-200 border-blue-800",
+    success: "bg-green-200 border-green-800",
+    dark: "bg-gray-800 text-white border-gray-300",
+  };
 
   const hoverStyles = hover ? "hover:bg-blue-700" : "";
 
-  return <div>{children}</div>;
+  return (
+    <div
+      className={`${baseStyles} ${variants[variant]} ${paddingStyles[padding]}`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Card;
